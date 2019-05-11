cca069f01c64a719122fc8987b13ceed5f235d6e
PeterBrue <peter.brue@gmail.com>
2019-05-11 16:02:57 -0500

    Created main, footer, and new pages

---
+++ app.js
@@ -0,0 +1,6 @@
+
+var date = new Date();
+var year = date.getFullYear();
+var footerYear = document.getElementById('currentYear');
+var textYear = document.createTextNode(year);
+footerYear.appendChild(textYear);
